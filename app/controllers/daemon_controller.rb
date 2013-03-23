4eed6765e1dc00ad67efa412172882063bbe48c0
Pierre Pfister <pierre@pfister-brachlianoff.fr>
2013-03-23 15:13:21 +0000

    links are working, no daemon yet

---
+++ app/controllers/daemon_controller.rb
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+class DaemonController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 ArduinoToWeb::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  get "welcome/index"
+  match "/welcome" => "welcome#index"
+  match "/daemon" => "daemon#index"
+
   root :to => 'welcome#index'
 
   # The priority is based upon order of creation:
@@ -10,7 +12,6 @@ ArduinoToWeb::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  match 'arduinoDaemon' => 'arduinoDaemon#index'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
