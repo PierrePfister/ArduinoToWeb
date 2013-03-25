5a1c2f86389c5a09f73f052f143c22c1acf6ef56
Pierre Pfister <pierre@pfister-brachlianoff.fr>
2013-03-25 14:03:19 +0000

    working but daemon still exiting

--- app/controllers/daemon_controller.rb
+++ app/controllers/daemon_controller.rb
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 
+require 'rubygems'
+require 'daemons'
+
 class DaemonController < ApplicationController
   def index
-  end
-end
+
+    options = {
+      :app_name => "collectTemperature",
+      :multiple => true
+    }
+
+    taskread = Daemons.call do      
+      loop {sleep 10}
+    end
+
+    
+  
+  end # def
+end # class
